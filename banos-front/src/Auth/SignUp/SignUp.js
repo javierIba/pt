80500ea3be08b895c5e7c4c35dd2d9b87a2554e5
javierIba <javier.i.ibanez.a@gmail.com>
2022-11-04 02:03:26 -0300

    ya le agregue los valdadores a signup

--- banos-front/src/Auth/SignUp/SignUp.js
+++ banos-front/src/Auth/SignUp/SignUp.js
@@ -14,12 +14,20 @@ export default function SignUp() {
         password: null,
         name: null
     });
-    const [errorMessage,setErrorMessage] = useState(null);
-    const emailRegx  = /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i;
-
-
+    const [isClick, setIsClick] = useState({
+        email: false,
+        password: false,
+        name: false
+    });
+    const [errorMessage, setErrorMessage] = useState(null);
+    const emailRegx = /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i;
+    const nameRegx = /^[a-zA-Z]+$/
+    const emailValidation = (userData.email !== null) && !emailRegx.test(userData.email) && isClick.email;
+    const passwordValidation = (userData.password !== null) && (6 >= userData.password.length) && isClick.password;
+    const nameValidation = (userData.name !== null) && !nameRegx.test(userData.name) && isClick.name;
 
     function handleInputsForm(e) {
+
         switch (e.target.name) {
             case "email":
                 setUserData({ ...userData, email: e.target.value });
@@ -35,28 +43,42 @@ export default function SignUp() {
         }
     }
 
-/*VALIDAR DATOS*/
-   
-    function handleForm(e){
+    /*VALIDAR DATOS*/
+
+    function handleForm(e) {
         e.preventDefault()
-        // if()
-        
+
+
     }
 
 
     return (<>
         <div className="login-page">
-            <div className="form">
 
-                <form className="register-form" onClick={(e) => handleForm(e)}>
-                    <input type="text" placeholder="Nombre" name="name" onChange={(e) => handleInputsForm(e)} required />
-                    <input type="password" placeholder="Contraseña" name="password" onChange={(e) => handleInputsForm(e)} required />
-                    <input type="email" placeholder="Email" name="email" onChange={(e) => handleInputsForm(e)} required/>
+            <div className="form">
+                <form className="register-form" onSubmit={(e) => handleForm(e)}>
+                    <div >
+                        <label className="input-label" for="signupName">Nombre</label>
+                        <input type="text" placeholder="Nombre" className={(nameValidation) && "error-input"} id="signupName" name="name" onChange={(e) => handleInputsForm(e)} onClick={() => setIsClick({ ...isClick, name: true })} required />
+                        {(nameValidation) && <p class="error-message">Nombre no valido, solo puede contener letras</p>}
+
+                    </div>
+                    <div>
+                        <label className="input-label" for="signupPassword">Contraseña</label>
+                        <input type="password" placeholder="Contraseña" id="signupPassword" name="password" onChange={(e) => handleInputsForm(e)} onClick={() => setIsClick({ ...isClick, password: true })} required />
+                        {(passwordValidation) && <p class="error-message">La contraseña debe tener más de 6 caracteres</p>}
+
+                    </div>
+                    <div>
+                        <label className="input-label" for="signupEmail">Email</label>
+                        <input type="email" placeholder="Email" id="signupEmail" name="email" onChange={(e) => handleInputsForm(e)} onClick={() => setIsClick({ ...isClick, email: true })} required />
+                        {(emailValidation) && <p class="error-message">Email no valido, debe cumplir con el siguiente formato: xxx@xxx.xxx</p>}
+                    </div>
+
                     <button >Registrarse</button>
                     <p className="message">¿Ya estas registrado? <a href="#">Inicia sesión</a></p>
                     <p className="error-message">{errorMessage}</p>
                 </form>
-
             </div>
         </div>
     </>)
