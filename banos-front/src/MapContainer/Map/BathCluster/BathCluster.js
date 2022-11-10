9e301b6fa25a93fd9411a8386b9d5bae4e94fa53
Daniel-gd7 <daniel.galvez1997@gmail.com>
2022-11-10 03:17:20 -0300

    update cluster baños

--- banos-front/src/MapContainer/Map/BathCluster/BathCluster.js
+++ banos-front/src/MapContainer/Map/BathCluster/BathCluster.js
@@ -47,8 +47,8 @@ const BathCluster = () => {
 
     const toiletExample = [{
         id: 1,
-        lng: -71.61775403926892,
-        lat: -33.046804349799366,
+        lng: -71.617666,
+        lat: -33.046555,
         free: true,
         disability_access: true,
         diaper_changing: true,
@@ -83,7 +83,330 @@ const BathCluster = () => {
             disability_access: true,
             diaper_changing: true
         }]
-    }]
+    },
+    {
+        id: 3,
+        lng: -71.620142,
+        lat: -33.046239,
+        free: true,
+        disability_access: true,
+        diaper_changing: true,
+
+        reviews: [{
+            id: 1,
+            username: "javier ibáñez",
+            calification: 4,
+            cleaning_calification: 3,
+            privacy_calification: 5,
+            free: true,
+            disability_access: true,
+            diaper_changing: true
+        }]
+
+    },
+    {
+        id: 4,
+        lng: -71.605019,
+        lat: -33.044000,
+        free: true,
+        disability_access: true,
+        diaper_changing: true,
+
+        reviews: [{
+            id: 1,
+            username: "javier ibáñez",
+            calification: 4,
+            cleaning_calification: 3,
+            privacy_calification: 5,
+            free: true,
+            disability_access: true,
+            diaper_changing: true
+        }]
+
+    },
+    {
+        id: 5,
+        lng: -71.605057,
+        lat: -33.043503,
+        free: true,
+        disability_access: true,
+        diaper_changing: true,
+
+        reviews: [{
+            id: 1,
+            username: "javier ibáñez",
+            calification: 4,
+            cleaning_calification: 3,
+            privacy_calification: 5,
+            free: true,
+            disability_access: true,
+            diaper_changing: true
+        }]
+
+    },
+    {
+        id: 6,
+        lng: -71.607492,
+        lat: -33.045016,
+        free: true,
+        disability_access: true,
+        diaper_changing: true,
+
+        reviews: [{
+            id: 1,
+            username: "javier ibáñez",
+            calification: 4,
+            cleaning_calification: 3,
+            privacy_calification: 5,
+            free: true,
+            disability_access: true,
+            diaper_changing: true
+        }]
+
+    },
+    {
+        id: 7,
+        lng: -71.627126,
+        lat: -33.039391,
+        free: true,
+        disability_access: true,
+        diaper_changing: true,
+
+        reviews: [{
+            id: 1,
+            username: "javier ibáñez",
+            calification: 4,
+            cleaning_calification: 3,
+            privacy_calification: 5,
+            free: true,
+            disability_access: true,
+            diaper_changing: true
+        }]
+
+    },
+    {
+        id: 8,
+        lng: -71.606608,
+        lat: -33.046894,
+        free: true,
+        disability_access: true,
+        diaper_changing: true,
+
+        reviews: [{
+            id: 1,
+            username: "javier ibáñez",
+            calification: 4,
+            cleaning_calification: 3,
+            privacy_calification: 5,
+            free: true,
+            disability_access: true,
+            diaper_changing: true
+        }]
+
+    },
+    {
+        id: 9,
+        lng: -71.613459,
+        lat: -33.047383,
+        free: true,
+        disability_access: true,
+        diaper_changing: true,
+
+        reviews: [{
+            id: 1,
+            username: "javier ibáñez",
+            calification: 4,
+            cleaning_calification: 3,
+            privacy_calification: 5,
+            free: true,
+            disability_access: true,
+            diaper_changing: true
+        }]
+
+    },
+    {
+        id: 10,
+        lng: -71.627471,
+        lat: -33.038108,
+        free: true,
+        disability_access: true,
+        diaper_changing: true,
+
+        reviews: [{
+            id: 1,
+            username: "javier ibáñez",
+            calification: 4,
+            cleaning_calification: 3,
+            privacy_calification: 5,
+            free: true,
+            disability_access: true,
+            diaper_changing: true
+        }]
+
+    },
+    {
+        id: 11,
+        lng: -71.627531,
+        lat: -33.037357,
+        free: true,
+        disability_access: true,
+        diaper_changing: true,
+
+        reviews: [{
+            id: 1,
+            username: "javier ibáñez",
+            calification: 4,
+            cleaning_calification: 3,
+            privacy_calification: 5,
+            free: true,
+            disability_access: true,
+            diaper_changing: true
+        }]
+
+    },
+    {
+        id: 12,
+        lng: -71.626340,
+        lat: -33.041703,
+        free: true,
+        disability_access: true,
+        diaper_changing: true,
+
+        reviews: [{
+            id: 1,
+            username: "javier ibáñez",
+            calification: 4,
+            cleaning_calification: 3,
+            privacy_calification: 5,
+            free: true,
+            disability_access: true,
+            diaper_changing: true
+        }]
+
+    },
+    {
+        id: 13,
+        lng: -71.607796,
+        lat: -33.041803,
+        free: true,
+        disability_access: true,
+        diaper_changing: true,
+
+        reviews: [{
+            id: 1,
+            username: "javier ibáñez",
+            calification: 4,
+            cleaning_calification: 3,
+            privacy_calification: 5,
+            free: true,
+            disability_access: true,
+            diaper_changing: true
+        }]
+
+    },
+    {
+        id: 14,
+        lng: -71.628412,
+        lat: -33.046781,
+        free: true,
+        disability_access: true,
+        diaper_changing: true,
+
+        reviews: [{
+            id: 1,
+            username: "javier ibáñez",
+            calification: 4,
+            cleaning_calification: 3,
+            privacy_calification: 5,
+            free: true,
+            disability_access: true,
+            diaper_changing: true
+        }]
+
+    },
+    {
+        id: 15,
+        lng: -71.62235,
+        lat: -33.04588,
+        free: true,
+        disability_access: true,
+        diaper_changing: true,
+
+        reviews: [{
+            id: 1,
+            username: "javier ibáñez",
+            calification: 4,
+            cleaning_calification: 3,
+            privacy_calification: 5,
+            free: true,
+            disability_access: true,
+            diaper_changing: true
+        }]
+
+    },
+    {
+        id: 16,
+        lng: -71.62467,
+        lat: -33.04692,
+        free: true,
+        disability_access: true,
+        diaper_changing: true,
+
+        reviews: [{
+            id: 1,
+            username: "javier ibáñez",
+            calification: 4,
+            cleaning_calification: 3,
+            privacy_calification: 5,
+            free: true,
+            disability_access: true,
+            diaper_changing: true
+        }]
+
+    },
+    {
+        id: 17,
+        lng: -71.62425,
+        lat: -33.04579,
+        free: true,
+        disability_access: true,
+        diaper_changing: true,
+
+        reviews: [{
+            id: 1,
+            username: "javier ibáñez",
+            calification: 4,
+            cleaning_calification: 3,
+            privacy_calification: 5,
+            free: true,
+            disability_access: true,
+            diaper_changing: true
+        }]
+
+    },
+    {
+        id: 18,
+        lng: -71.62497,
+        lat: -33.04621,
+        free: true,
+        disability_access: true,
+        diaper_changing: true,
+
+        reviews: [{
+            id: 1,
+            username: "javier ibáñez",
+            calification: 4,
+            cleaning_calification: 3,
+            privacy_calification: 5,
+            free: true,
+            disability_access: true,
+            diaper_changing: true
+        }]
+
+    }
+
+
+]
 
     function handleOffCanvas() {
 
