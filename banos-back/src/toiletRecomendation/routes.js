2de259f3e3adb600d842dc95b589b17f1a85a19c
javierIba <82604865+javierIba@users.noreply.github.com>
2024-06-04 05:33:55 -0400

    Update routes.js

--- banos-back/src/toiletRecomendation/routes.js
+++ banos-back/src/toiletRecomendation/routes.js
@@ -6,5 +6,5 @@ const router = Router();
 router.post('/toiletAdd', validateToken, toiletAdd);
 router.post('/getcollection', validateToken, getAllOfCollection);
 router.post('/deletedoc', validateToken, deleteDoc);
-router.post('/toiletrecomendationtotoilets', validateToken, addToiletRecomendationToToilets);
+router.post('/toiletrecomendationtotoilets', validateToken,  addToiletRecomendationToToilets);
 module.exports = router;
